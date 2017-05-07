50e98d573d651a928b07be603c4b6fdd4165dcd7
Benjamin Saunders <ben.e.saunders@gmail.com>
2017-05-06 19:06:51 -0700

    Build OpenVR client lib from source and statically link

--- build.rs
+++ build.rs
@@ -1,17 +1,10 @@
-#[cfg(target_os="macos")]
-fn main() {
-    println!("cargo:rustc-link-search={}/openvr/lib/osx32", env!("CARGO_MANIFEST_DIR"));
-    println!("cargo:rustc-link-search={}/openvr/bin/osx32", env!("CARGO_MANIFEST_DIR"));
-}
+extern crate cmake;
 
-#[cfg(target_os="linux")]
 fn main() {
-    println!("cargo:rustc-link-search={}/openvr/lib/linux64", env!("CARGO_MANIFEST_DIR"));
-    println!("cargo:rustc-link-search={}/openvr/bin/linux64", env!("CARGO_MANIFEST_DIR"));
-}
+    let dst = cmake::build("openvr");
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=static=openvr_api");
 
-#[cfg(target_os="windows")]
-fn main() {
-    println!("cargo:rustc-link-search={}/openvr/lib/win64", env!("CARGO_MANIFEST_DIR"));
-    println!("cargo:rustc-link-search={}/openvr/bin/win64", env!("CARGO_MANIFEST_DIR"));
+    #[cfg(target_os="linux")]
+    println!("cargo:rustc-link-lib=stdc++");
 }
