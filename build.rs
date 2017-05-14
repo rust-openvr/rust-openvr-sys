68c515469e7d94b30d205f0046e0987df040e94f
Benjamin Saunders <ben.e.saunders@gmail.com>
2017-05-13 20:06:37 -0700

    Fix linking on windows

--- build.rs
+++ build.rs
@@ -9,6 +9,11 @@ fn main() {
 
     let dst = cfg.build();
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
+
+    #[cfg(all(windows, target_pointer_width = "64"))]
+    println!("cargo:rustc-link-lib=static=openvr_api64");
+
+    #[cfg(not(all(windows, target_pointer_width = "64")))]
     println!("cargo:rustc-link-lib=static=openvr_api");
 
     #[cfg(target_os="linux")]
