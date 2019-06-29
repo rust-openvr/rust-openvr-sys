2a1f957c386d398028d6095d771dc2e75b9477a5
Christoph Schulz <schulzcbs@gmail.com>
2019-06-29 22:43:13 +0200

    Cleanup

--- lib.rs
+++ lib.rs
@@ -1,9 +1,7 @@
-#![allow(non_camel_case_types,
-         non_upper_case_globals,
-         non_snake_case)]
+#![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
 
 include!("bindings.rs");
 
-#[cfg(target_os="macos")]
+#[cfg(target_os = "macos")]
 #[link(name = "Foundation", kind = "framework")]
-extern { }
+extern "C" {}
