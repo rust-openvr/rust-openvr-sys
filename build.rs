50681dd4a11863076a1f34589dc4978b3eff96b4
Christoph Schulz <schulzcbs@gmail.com>
2019-06-29 22:42:18 +0200

    Rework build.rs to use CARGO_* environment variables, fixed missing "shell32" (windows), and added "buildtime_bindgen" as an optional feature to generate bindings at compile time.

--- build.rs
+++ build.rs
@@ -1,27 +1,46 @@
+#[cfg(feature = "buildtime_bindgen")]
+extern crate bindgen;
 extern crate cmake;
 
+use std::env;
+
 fn main() {
-    let mut cfg = cmake::Config::new("openvr");
+    let mut config = cmake::Config::new("openvr");
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let target_pointer_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap();
 
-    // Work around broken cmake build
-    #[cfg(windows)]
-    cfg.cxxflag("/DWIN32");
+    if target_os == "macos" {
+        config.define("BUILD_UNIVERSAL", "OFF");
+    } else if target_os == "windows" {
+        // Work around broken cmake build.
+        config.cxxflag("/DWIN32");
+    }
 
-    #[cfg(target_os="macos")]
-    cfg.define("BUILD_UNIVERSAL", "OFF");
-
-    let dst = cfg.build();
+    let dst = config.build();
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
 
-    #[cfg(all(windows, target_pointer_width = "64"))]
-    println!("cargo:rustc-link-lib=static=openvr_api64");
+    if target_os == "windows" && target_pointer_width == "64" {
+        println!("cargo:rustc-link-lib=static=openvr_api64");
+    } else {
+        println!("cargo:rustc-link-lib=static=openvr_api");
+    }
 
-    #[cfg(not(all(windows, target_pointer_width = "64")))]
-    println!("cargo:rustc-link-lib=static=openvr_api");
+    if target_os == "linux" {
+        println!("cargo:rustc-link-lib=stdc++");
+    } else if target_os == "macos" {
+        println!("cargo:rustc-link-lib=c++");
+    } else if target_os == "windows" {
+        println!("cargo:rustc-link-lib=shell32");
+    }
 
-    #[cfg(target_os="linux")]
-    println!("cargo:rustc-link-lib=stdc++");
-
-    #[cfg(target_os="macos")]
-    println!("cargo:rustc-link-lib=c++");
+    // Generate bindings at build time.
+    #[cfg(feature = "buildtime_bindgen")]
+    bindgen::builder()
+        .header("wrapper.hpp")
+        .constified_enum(".*")
+        .prepend_enum_name(false)
+        .generate()
+        .expect("could not generate bindings")
+        .write_to_file("bindings.rs")
+        .expect("could not write bindings.rs");
 }
