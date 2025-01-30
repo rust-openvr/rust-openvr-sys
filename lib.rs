ebca3aa10ce500f29d57d1c31de56ad5280c56fe
Alexander Brook Perry <alex@alexgeek.co.uk>
2025-01-30 15:24:58 +0000

    Move generated files into OUT_DIR as cargo package fails if we don't.

--- lib.rs
+++ lib.rs
@@ -1,6 +1,6 @@
 #![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
 
-include!("bindings.rs");
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(target_os = "macos")]
 #[link(name = "Foundation", kind = "framework")]
