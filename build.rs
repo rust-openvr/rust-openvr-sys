680ebe64a2ecb439c7873adaead113ae8ebd0efa
Alexander Brook Perry <alex@alexgeek.co.uk>
2025-01-30 16:26:17 +0000

    Fix packaging errors due to modifying outside of OUT_DIR (#16)
    * Move generated files into OUT_DIR as cargo package fails if we don't.
    
    * Workaround for forced paths in openvr CMakeLists.txt.

--- build.rs
+++ build.rs
@@ -1,12 +1,30 @@
-extern crate bindgen;
-extern crate cmake;
-
+use bindgen;
+use cmake;
 use std::env;
+use std::path::PathBuf;
 
 fn main() {
-    let mut config = cmake::Config::new("openvr");
+    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("Missing OUT_DIR env var"));
+
+    println!("cargo:rerun-if-changed=wrapper.hpp");
+
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     let target_pointer_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap();
+
+    // Configure cmake to place build output in OUT_DIR
+    let out_dir_str = out_dir.to_string_lossy().into_owned();
+    let mut config = cmake::Config::new("openvr");
+    let config = config
+        .define("CMAKE_LIBRARY_OUTPUT_DIRECTORY", &out_dir_str)
+        .define("CMAKE_ARCHIVE_OUTPUT_DIRECTORY", &out_dir_str)
+        .define("CMAKE_RUNTIME_OUTPUT_DIRECTORY", &out_dir_str)
+        .define("CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG", &out_dir_str)
+        .define("CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE", &out_dir_str)
+        .define("CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG", &out_dir_str)
+        .define("CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE", &out_dir_str)
+        .define("CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG", &out_dir_str)
+        .define("CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE", &out_dir_str)
+        .out_dir(&out_dir); 
 
     if target_os == "macos" {
         config.define("BUILD_UNIVERSAL", "OFF");
@@ -16,6 +34,7 @@ fn main() {
     }
 
     let dst = config.build();
+
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
 
     if target_os == "windows" && target_pointer_width == "64" {
@@ -32,12 +51,13 @@ fn main() {
         println!("cargo:rustc-link-lib=shell32");
     }
 
+    // Generate bindings and write them into OUT_DIR
     bindgen::builder()
         .header("wrapper.hpp")
         .constified_enum(".*")
         .prepend_enum_name(false)
         .generate()
         .expect("could not generate bindings")
-        .write_to_file("bindings.rs")
+        .write_to_file(out_dir.join("bindings.rs"))
         .expect("could not write bindings.rs");
 }
